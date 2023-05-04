2870b7dcbb560f829f9632bcef3ece343c07f393
Camilo Santander <camilo.santander1991@gmail.com>
2023-05-04 15:46:26 -0400

    eslint added

--- app.js
+++ app.js
@@ -5,18 +5,7 @@ const routes = require('./routes/routes');
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET,POST,PUT,DELETE,OPTIONS',
-    );
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Access-Control-Allow-Headers',
-    );
-    next();
-});
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,23 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const productsController = require('../controllers/productsController');
-const {
-    getCountries,
-    getCountryById,
-    createCountry,
-    updateCountry,
-    deleteCountry,
-} = require('../controllers/countriesController');
+const countriesController = require('../controllers/countriesController');
 
 router.get('/products', productsController.getProducts);
 router.post('/products', productsController.createProduct);
 router.put('/products/:id', productsController.updateProduct);
 router.delete('/products/:id', productsController.deleteProduct);
 
-router.get('/countries', getCountries);
-router.get('/countries/:id', getCountryById);
-router.post('/countries', createCountry);
-router.put('/countries/:id', updateCountry);
-router.delete('/countries/:id', deleteCountry);
+router.get('/countries', countriesController.getCountries);
+router.get('/countries/:id', countriesController.getCountryById);
+router.post('/countries', countriesController.createCountry);
+router.put('/countries/:id', countriesController.updateCountry);
+router.delete('/countries/:id', countriesController.deleteCountry);
 
 module.exports = router;
