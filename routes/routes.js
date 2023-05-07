1fc63d02626d875b60fe72326af38378561b4da8
Camilo Santander <camilo.santander1991@gmail.com>
2023-05-06 22:23:12 -0400

    minimum changes

--- routes/routes.js
+++ routes/routes.js
@@ -6,18 +6,21 @@ const countriesController = require('../controllers/countriesController');
 const regionsController = require('../controllers/regionsController');
 const addingCountries = require('../utilities/countryAdder');
 
+// Products
 router.get('/products', productsController.getProducts);
 router.get('/products/:id', productsController.getProductById);
 router.post('/products', productsController.createProduct);
 router.put('/products/:id', productsController.updateProduct);
 router.delete('/products/:id', productsController.deleteProduct);
 
+// Countries
 router.get('/countries', countriesController.getCountries);
 router.get('/countries/:id', countriesController.getCountryById);
 router.post('/countries', countriesController.createCountry);
 router.put('/countries/:id', countriesController.updateCountry);
 router.delete('/countries/:id', countriesController.deleteCountry);
 
+// Regions
 router.get('/regions', regionsController.getRegions);
 router.get('/regions/:id', regionsController.getRegionById);
 router.post('/regions', regionsController.createRegion);
