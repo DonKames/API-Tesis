ef749bee560febcb2cdbaae9575d1cc854457d64
Camilo Santander <camilo.santander1991@gmail.com>
2023-08-21 02:48:42 -0400

    working on settings

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -3,14 +3,15 @@ const handleErrors = require('../middlewares/errorHandler');
 
 const getGlobalSettings = async (req, res) => {
     const response = await db.query('SELECT * FROM global_settings');
-    res.status(200).json(response.rows);
+    res.status(200).json(response.rows[0]);
 };
 
 const createGlobalSettings = async (req, res) => {
-    const { name, value } = req.body;
+    console.log(req.body);
+    const idWarehouse = req.body.warehouse_id;
     const response = await db.query(
         'INSERT INTO global_settings (main_warehouse) VALUES ($1) RETURNING *',
-        [name, value],
+        [idWarehouse],
     );
     res.status(201).json(response.rows[0]);
 };
